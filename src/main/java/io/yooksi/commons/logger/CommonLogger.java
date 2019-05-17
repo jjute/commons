eb0eb357d97ed39a0b93c858b54fc14e735af26a
yooksi <yooksi@zoho.com>
2019-05-17 20:58:07 +0200

    Connect CommonLogger with Log4j config

--- src/main/java/io/yooksi/commons/logger/CommonLogger.java
+++ src/main/java/io/yooksi/commons/logger/CommonLogger.java
@@ -4,55 +4,159 @@ import io.yooksi.commons.define.MethodsNotNull;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import javax.validation.constraints.NotEmpty;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
+import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 
 @MethodsNotNull
-@SuppressWarnings("unused")
-public class CommonLogger extends ICommonLogger {
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class CommonLogger extends AbsCommonLogger {
 
-    /* Used for internal library logging */
-    private static CommonLogger instance = new CommonLogger();
+    private static final Logger INTER_LOGGER = LogManager.getLogger("ykcommons-internal");
 
-    /**
-     * Create a new {@code CommonLogger} implementation.
-     *
-     * @param name the Logger name used to create a new log4j
-     * @param implClass implementation class to instantiate
-     * @param <T> {@link CommonLogger} subclass
-     * @return newly instantiated implementation
-     * @throws IllegalStateException if the class has no nullary constructor
-     * or if the class or its nullary constructor is not accessible.
-     */
-    public static <T extends CommonLogger> T create(@NotEmpty String name, Class<T> implClass) {
+    public final LoggerContext context;
+    private final LoggerConfig loggerConfig;
+    private final FileAppender fileAppender;
 
-        try {
-            T impl = implClass.newInstance();
-            impl.logger = LogManager.getLogger(name);
-            instance.debug("Created new logger for %s", impl);
-            return impl;
-        }
-        catch (InstantiationException | IllegalAccessException e) {
-            throw new IllegalStateException(e);
-        }
+    private final Logger logger;
+    private final Level logLevel;
+    private Level logFileLevel;
+
+    public CommonLogger(String logger, Level logLevel, Level logFileLevel) {
+
+        INTER_LOGGER.printf(Level.DEBUG, "Initializing new CommonLogger %s " +
+                "with level " + "%s(log), %s(file)", logger, logLevel, logFileLevel);
+
+        this.logLevel = logLevel;
+        this.logFileLevel = logFileLevel;
+
+        context = (LoggerContext) LogManager.getContext(false);
+        Configuration config = context.getConfiguration();
+
+        INTER_LOGGER.printf(Level.DEBUG, "Using %s Configuration", config.getName());
+
+        boolean existsLogger = config.getLoggers().containsKey(logger);
+        INTER_LOGGER.printf(Level.DEBUG, existsLogger ? "Getting logger %s from current context"
+                : "Unable to find logger %s in current context, creating new one with default configs", logger);
+        /*
+         * If no existing logger configuration was found for the parameter name
+         * then the root logger configuration will be used instead
+         */
+        this.logger = LogManager.getLogger(logger);
+        loggerConfig = context.getConfiguration().getLoggerConfig(this.logger.getName());
+
+        ConsoleAppender consoleAppender = Log4jUtils.getOrInitConsoleAppender(this);
+        fileAppender = Log4jUtils.getOrInitFileAppender(this, consoleAppender.getLayout(), "logs/" + logger);
+
+        loggerConfig.setAdditive(false);
+        loggerConfig.setLevel(logLevel);
+        /*
+         * This causes all Loggers to re-fetch information from their LoggerConfig.
+         * We have to call this if we want to see our changes take place
+         */
+        context.updateLoggers();
+        /*
+         * Currently used only by tests but is useful to all users
+         * TODO: implement a system that creates new log files on each
+         *  run and packs the ones from day before into a zipped archive
+         */
+        clearLogFile();
     }
+    public CommonLogger(String logger, Level logLevel, boolean logToFile) {
+        this(logger, logLevel, logToFile ? logLevel : Level.OFF);
+    }
+
+    public org.apache.logging.log4j.core.Logger getCoreLogger() {
+        return ((org.apache.logging.log4j.core.Logger)logger);
+    }
+    public Level getLogLevel() {
+        return logLevel;
+    }
+
     /**
-     * Call this from an {@code ICommonLogger} implementation
-     * when you need access to more logging methods.
+     * @return Logger level assigned to this logger's FileAppender.
+     */
+    public Level getLogFileLevel() {
+        return logFileLevel;
+    }
+
+    /**
+     * <p>{@code LoggerConfigs} are defined as {@code <logger>} blocks in the {@code log4j2.xml}
+     * {@code Configuration} file and are defined for each dedicated logger name.</p>
      *
-     * @return a instance of log4j logger created for this implementation
-     * of {@code ICommonLogger}
+     * If no {@code LoggerConfig} dedicated configuration definition was found
+     * in the XML file for our logger the root logger config will be used instead.
+     *
+     * @return dedicated logger configuration entry from {@code log4j2.xml} or root
+     * configuration entry if no dedicated config for our logger was found.
+     */
+    public LoggerConfig getLoggerConfig() {
+        return loggerConfig;
+    }
+
+    /**
+     * Call this from an {@code AbsCommonLogger} implementation when you
+     * need access to more logging methods. Can also be called from
+     * utility methods when the logger instance is not exposed.
+     *
+     * @return an instance of {@code Log4j} logger created for this implementation
+     * or the internal logger used as fallback when the logger is still initializing
      */
     public final Logger getLogger() {
-        return logger;
+        return logger != null ? logger : INTER_LOGGER;
     }
-    /**
-     * Internal method used by library classes for logging purposes.
-     * @return default instance of {@code CommonLogger}.
-     */
-    public static ICommonLogger get() {
-        return instance;
+
+    public java.io.File getLogFile() {
+        return new java.io.File(fileAppender.getFileName());
     }
+
+    public void clearLogFile() {
+
+        try {
+            java.io.PrintWriter writer = new java.io.PrintWriter(getLogFile());
+            writer.print(""); writer.close();
+        }
+        catch (java.io.FileNotFoundException e) {
+            error("Unable to clear logfile", e);
+        }
+    }
+
+    public void startLoggingToFile(Level level) {
+
+        //loggerConfig.removeAppender(fileAppender.getName());
+        loggerConfig.addAppender(fileAppender, level, null);
+        //context.updateLoggers();
+        logFileLevel = level;
+
+        //debug("%s started logging to file with level %s", logger.getName(), level);
+    }
+    public void startLoggingToFile() {
+
+        //loggerConfig.removeAppender(fileAppender.getName());
+        loggerConfig.addAppender(fileAppender, logFileLevel, null);
+        //context.updateLoggers();
+
+        //debug("%s started logging to file with level %s", logger.getName(), logFileLevel);
+    }
+
+    public void stopLoggingToFile() {
+
+        loggerConfig.removeAppender(fileAppender.getName());
+        //context.updateLoggers();
+
+        //debug("%s stopped logging to file with level %s", logger.getName(), logFileLevel);
+    }
+
+    @Override
+    public void finalize() throws Throwable {
+
+        info("Finalizing %s and removing logger %s",this, logger.getName());
+        context.getConfiguration().removeLogger(logger.getName());
+        super.finalize();
+    }
+
     /*
      * Short-hand methods to print longs to console.
      */
