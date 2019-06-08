9e2630e3ffd02d0999f634edaf4f51e84d2d25aa
yooksi <yooksi@zoho.com>
2019-06-08 20:02:37 +0200

    Write InitializationPackage documentation

--- src/main/java/io/yooksi/commons/logger/InitializationPackage.java
+++ src/main/java/io/yooksi/commons/logger/InitializationPackage.java
@@ -8,6 +8,13 @@ import org.apache.logging.log4j.core.config.LoggerConfig;
 
 import java.io.Serializable;
 
+/**
+ * Extended appender data container that holds information
+ * used when retrieving, initializing or creating new appenders.
+ *
+ * @param <T> Appender implementation type
+ * @see Log4jUtils#getOrSetupAppender(InitializationPackage)
+ */
 class InitializationPackage<T extends Appender> {
 
     final LoggerControl loggerControl;
@@ -60,16 +67,32 @@ class InitializationPackage<T extends Appender> {
         names = type.getNames();
     }
 
+    /**
+     * Check if this initialization package represents a file appender that wants to
+     * print to a dedicated log file relative to the given {@code AppenderData}.
+     *
+     * @return {@code true} if this initialization package represents a file appender type that
+     *         has a <i>(non-empty)</i> log file path that is <b>different</b> then that resolved
+     *         from the given {@code AppenderData}.
+     */
     public boolean isDedicatedFileAppender(AppenderData<T> data) {
 
         return type == AppenderType.FILE && !logFilePath.isEmpty() && !logFilePath.equals(
                 Log4jUtils.getLogFilePath(AppenderType.FILE.getTypeClass().cast(data.getAppender())));
     }
 
+    /**
+     * Creates and returns a new initialization package copy using the given layout.
+     * The new package will have all of it's properties copied over except the layout.
+     */
     public InitializationPackage<T> copyWithLayout(Layout<? extends Serializable> layout) {
         return create(loggerControl, type).forFileAppender(layout, logFilePath).build();
     }
 
+    /**
+     * Create and return a new package builder for the given {@code AppenderType}.
+     * @param control {@code LoggerControl} used to manage this appender type
+     */
     public static <T extends Appender> Builder<T> create(LoggerControl control, AppenderType<T> type) {
         return new Builder<>(control, type, control.getLevel(type.getLevelType()));
     }
