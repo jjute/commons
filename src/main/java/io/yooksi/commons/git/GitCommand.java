a4ee3ccee6b62b79ace1e0fe04db0cd17b36ee86
yooksi <yooksi@zoho.com>
2019-06-27 00:24:14 +0200

    Create Git command classes

---
+++ src/main/java/io/yooksi/commons/git/GitCommand.java
@@ -0,0 +1,47 @@
+package io.yooksi.commons.git;
+
+import io.yooksi.commons.bash.BashCommand;
+import org.jetbrains.annotations.Contract;
+
+import java.util.Arrays;
+
+public abstract class GitCommand extends BashCommand {
+
+    private final GitCLOption[] options;
+
+    GitCommand(String format, String[] args, GitCLOption... options) {
+        super(Type.GIT, formatCommand(format, args, options));
+        this.options = options;
+    }
+
+    GitCommand(String cmd, GitCLOption... options) {
+        super(Type.GIT, cmd.replace("%opts", consolidateOptions(options)));
+        this.options = options;
+    }
+
+    private static String formatCommand(String format, String[] args, GitCLOption... options) {
+
+        format = format.replace("%opts", consolidateOptions(options));
+
+        for (GitCLOption option : options) {
+            format = format.replaceFirst("%opt", option.toString());
+        }
+        return args.length > 0 ? String.format(format, (Object[]) args) : format;
+    }
+
+    private static String consolidateOptions(GitCLOption... options) {
+
+        if (options.length > 0)
+        {
+            StringBuilder sb = new StringBuilder();
+            Arrays.stream(options).forEach(o -> sb.append(o.toString()).append(" "));
+            return sb.toString();
+        }
+        else return "";
+    }
+
+    @Contract(pure = true)
+    public GitCLOption[] getOptions() {
+        return options;
+    }
+}
