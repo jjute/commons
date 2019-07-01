240104c8eb69c04504fd98dbe627b0a8cba0a726
yooksi <yooksi@zoho.com>
2019-07-01 22:41:14 +0200

    Resolve GitOptionsTest failiure

--- src/test/java/commons/GitOptionsTest.java
+++ src/test/java/commons/GitOptionsTest.java
@@ -1,8 +1,11 @@
 package commons;
 
 import io.yooksi.commons.git.DiffFilterOption;
+import org.jetbrains.annotations.TestOnly;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 @SuppressWarnings("WeakerAccess")
 public class GitOptionsTest {
@@ -16,12 +19,17 @@ public class GitOptionsTest {
 
         DiffFilterOption.Type[] types = { DiffFilterOption.Type.ADDED, DiffFilterOption.Type.COPIED };
         option = DiffFilterOption.create().include(types).build();
-        Assertions.assertEquals("AC", option.getValue());
+        shouldContainDiffFilterOptions(option.getValue(), 'A', 'C');
 
         option = DiffFilterOption.create().include(types).exclude(DiffFilterOption.Type.RENAMED).build();
-        Assertions.assertEquals("ACr", option.getValue());
+        shouldContainDiffFilterOptions(option.getValue(), 'A', 'C', 'r');
 
         option = DiffFilterOption.create().include(types).setAllOrNone(true).build();
-        Assertions.assertEquals("AC*", option.getValue());
+        shouldContainDiffFilterOptions(option.getValue(), 'A', 'C', '*');
+    }
+
+    @TestOnly
+    public void shouldContainDiffFilterOptions(String value, Character... options) {
+        Arrays.stream(options).forEach(o -> Assertions.assertTrue(value.contains(Character.toString(o))));
     }
 }
